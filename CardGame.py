09a509b42d27d6c5de1d0781099301c3d691726b
Ryan Sands <sandsryanj@gmail.com>
2025-03-03 13:19:54 -0800

    class skeletons

--- CardGame.py
+++ CardGame.py
@@ -2,7 +2,97 @@
 Some description of each of the cards/classes implemented for this game
 """
 
+class Player():
+    """
+    Player class that represents a player in the game. Each player has a hand of cards, mana...
+    """
+    def __init__(self, name: str, deck=25, maxHP=20, maxMana=0, shield=0):
+        self.name = name
+        self.shield = shield
+        self.maxHP = maxHP
+        self.HP = maxHP
+        self.maxMana = maxMana
+        self.mana = maxMana
+        self.deck = deck
+        self.hand = []
 
+    def __str__(self):
+        return f"{self.name} has {self.HP}/{self.maxHP} HP and {self.mana}/{self.maxMana} mana"
+
+    def drawCards(self, numCards: int):
+        pass
+
+    def playCard(self):
+        pass
+
+    def isAlive(self):
+        if self.HP <= 0:
+            return False
+        elif (self.deck <= 0) and (len(self.hand) == 0):
+            return False
+        return True
+
+class Card():
+    """
+    Card class that represents a card in the game. Each card has a name, a description, and a cost.
+    """
+    def __init__(self, name: str, description: str, cost: int):
+        self.name = name
+        self.cost = cost
+        self.description = description
+
+    def __str__(self):
+        return f"{self.name}: {self.description}\nMana cost: {self.cost}"
+
+    def __eq__(self, other):
+        return self.cost == other.cost
+
+    def __lt__(self, other):
+        return self.cost < other.cost
+
+    def __gt__(self, other):
+        return self.cost > other.cost
+
+    def __le__(self, other):
+        return self.cost <= other.cost
+
+    def __ge__(self, other):
+        return self.cost >= other.cost
+
+    def __ne__(self, other):
+        return self.cost != other.cost
+    
+class UnitCard(Card):
+    """
+    UnitCard class that represents a playable unit in the game. Each card has a name, a description, a cost, an attack value, and health.
+    """
+    def __init__(self, name: str, description: str, cost: int, attack: int, maxHP: int):
+        super().__init__(name, description, cost)
+        self.attack = attack
+        self.maxHP = maxHP
+        self.HP = maxHP
+
+    def __str__(self):
+        return f"{self.name}: {self.description}\nMana cost: {self.cost}\nAttack: {self.attack}\nHP: {self.HP}/{self.maxHP}"
+    
+    def attack(self, target):
+        pass
+    
+    def cast(self):
+        pass
+
+class SpellCard(Card):
+    """
+    SpellCard class that represents a playable spell in the game. Each card has a name, a description, a cost, and an effect.
+    """
+    def __init__(self, name: str, description: str, cost: int):
+        super().__init__(name, description, cost)
+
+    def __str__(self):
+        return f"{self.name}: {self.description}\nMana cost: {self.cost}"
+    
+    def cast(self):
+        pass
 
 if __name__ == "__main__":
     pass
